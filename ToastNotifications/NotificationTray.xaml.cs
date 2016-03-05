5259ac9e20e64bcc8f5768c47f16e4cb6e986f85
Rafał Łopatka <alzur@outlook.com>
2016-03-05 15:10:06 +0100

    init

---
+++ ToastNotifications/NotificationTray.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToastNotifications
+{
+    /// <summary>
+    /// Interaction logic for NotificationTray.xaml
+    /// </summary>
+    public partial class NotificationTray : UserControl
+    {
+        public static readonly DependencyProperty NotificationsSourceProperty = DependencyProperty.Register("NotificationsSource", typeof(NotificationsSource), typeof(NotificationTray), new PropertyMetadata(default(NotificationsSource), NotificationSourceChanged));
+
+        private static void NotificationSourceChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var control = dependencyObject as NotificationTray;
+            var viewModel = eventArgs.NewValue as NotificationsSource;
+
+            if (control == null || viewModel == null)
+                return;
+
+            control._viewModel = viewModel;
+            control.DataContext = viewModel;
+        }
+
+        private Window _window;
+        private NotificationsSource _viewModel;
+
+        public NotificationTray()
+        {
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                Popup.IsOpen = false;
+            }
+            else
+            {
+                Loaded += OnLoaded;
+                Unloaded += OnUnloaded;
+            }
+        }
+
+        public NotificationsSource NotificationsSource
+        {
+            get { return (NotificationsSource) GetValue(NotificationsSourceProperty); }
+            set { SetValue(NotificationsSourceProperty, value); }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _window = Window.GetWindow(this);
+            
+            _window.SizeChanged += WindowOnSizeChanged;
+            _window.LocationChanged += WindowOnLocationChanged;
+            _window.StateChanged += WindowOnStateChanged;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _window.SizeChanged -= WindowOnSizeChanged;
+            _window.LocationChanged -= WindowOnLocationChanged;
+            _window.StateChanged -= WindowOnStateChanged;
+        }
+
+        private void WindowOnStateChanged(object sender, EventArgs eventArgs)
+        {
+            UpdateBounds();
+        }
+
+        private void WindowOnLocationChanged(object sender, EventArgs eventArgs)
+        {
+            UpdateBounds();
+        }
+
+        private void WindowOnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
+        {
+            UpdateBounds();
+        }
+
+        private void Notification_OnNotificationClosed(object sender, RoutedEventArgs e)
+        {
+            var control = sender as NotificationControl;
+
+            if (control == null)
+                return;
+
+            _viewModel.Hide(control.Notification.Id);
+
+            UpdateBounds();
+        }
+
+        private void UpdateBounds()
+        {
+            Popup.HorizontalOffset += 1;
+            Popup.HorizontalOffset -= 1;
+        }
+    }
+}
---
+++ ToastNotifications/NotificationsSource.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace ToastNotifications
+{
+    public class NotificationsSource : INotifyPropertyChanged
+    {
+        private readonly DispatcherTimer _timer;
+        private bool _isOpen;
+
+        public ObservableCollection<NotificationViewModel> NotificationMessages { get; private set; }
+
+        public long MaximumNotificationCount { get; set; }
+
+        public TimeSpan NotificationLifeTime { get; set; }
+
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            set
+            {
+                _isOpen = value;
+                OnPropertyChanged("IsOpen");
+            }
+        }
+
+        public NotificationsSource()
+        {
+            NotificationMessages = new ObservableCollection<NotificationViewModel>();
+
+            MaximumNotificationCount = 5;
+            NotificationLifeTime = TimeSpan.FromSeconds(6);
+
+            _timer = new DispatcherTimer(DispatcherPriority.Normal);
+            _timer.Interval = TimeSpan.FromMilliseconds(200);
+        }
+
+        private void TimerOnTick(object sender, EventArgs eventArgs)
+        {
+            var currentTime = DateTime.Now;
+
+            var itemsToRemove = NotificationMessages.Where(x => currentTime - x.CreateTime >= NotificationLifeTime).Select(x => x.Id).ToList();
+
+            foreach (var id in itemsToRemove)
+            {
+                Hide(id);
+            }
+        }
+
+        public void Show(string message, NotificationType type)
+        {
+            if (NotificationMessages.Any() == false)
+            {
+                InternalStartTimer();
+                IsOpen = true;
+            }
+
+            if (NotificationMessages.Count >= MaximumNotificationCount)
+            {
+                int removeCount = (int) (NotificationMessages.Count - MaximumNotificationCount) + 1;
+                
+                var itemsToRemove = NotificationMessages.OrderBy(x => x.CreateTime).Take(removeCount).Select(x => x.Id).ToList();
+                foreach (var id in itemsToRemove)
+                    Hide(id);
+            }
+
+            NotificationMessages.Add(new NotificationViewModel { Message = message, Type = type });
+        }
+
+        public void Hide(Guid id)
+        {
+            var n = NotificationMessages.Single(x => x.Id == id);
+            n.InvokeHideAnimation();
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(200);
+            }).ContinueWith(t =>
+            {
+                NotificationMessages.Remove(n);
+
+                if (NotificationMessages.Any() == false)
+                {
+                    InternalStopTimer();
+                    IsOpen = false;
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void InternalStartTimer()
+        {
+            _timer.Tick += TimerOnTick;
+            _timer.Start();
+        }
+
+        private void InternalStopTimer()
+        {
+            _timer.Stop();
+            _timer.Tick -= TimerOnTick;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
